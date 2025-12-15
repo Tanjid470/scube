f0e7549cc7758dd72ca456105c6b3b382f008d23
Tanjid1470 <tanjid@tutorsplan.com>
2025-12-15 18:56:17 +0600

    [FEAT]: Add app routes and pages configuration

--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/animation.dart';
+import 'package:get/get.dart';
+import 'package:scube/feature/home/presentation/home_view.dart';
+import 'package:scube/feature/login/presentation/login_view.dart';
+import 'package:scube/main.dart';
 part 'app_routes.dart';
 
 class AppPages{
   AppPages._();
-  //
-  // static String INITIAL = isWeb == true
-  //     ? RouteNames.webApp
-  //     : isInitScreen == 0 || isInitScreen == null
-  //     ? RouteNames.onboardView
-  //     : RouteNames.bottomNavigationWidget;
-  //
-  // static final routes =[
-  //   GetPage(
-  //     name:_Paths.bottomNavigationWidget,
-  //     page: () => const BottomNavigationWidget(),
-  //     transition: Transition.leftToRight,
-  //     curve: Curves.fastOutSlowIn,
-  //   ),
-  // ];
+
+  static String INITIAL = isInitScreen == 0 || isInitScreen == null
+      ? RouteNames.loginView
+      : RouteNames.homeView;
+
+  static final routes =[
+    GetPage(
+      name:_Paths.homeView,
+      page: () => const HomeView(),
+      transition: Transition.leftToRight,
+      curve: Curves.fastOutSlowIn,
+    ),
+    GetPage(
+      name:_Paths.loginView,
+      page: () => const LoginView(),
+      transition: Transition.leftToRight,
+      curve: Curves.fastOutSlowIn,
+    ),
+  ];
 }
--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -4,9 +4,11 @@ abstract class RouteNames {
   RouteNames._();
 
   static const loginView = _Paths.loginView;
+  static const homeView = _Paths.homeView;
 }
 
 abstract class _Paths {
   _Paths._();
   static const loginView = '/login_view';
+  static const homeView = '/home_view';
 }
