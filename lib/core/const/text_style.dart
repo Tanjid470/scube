bc43e951ac14e81614aee28052158678092f106a
Tanjid1470 <tanjid@tutorsplan.com>
2025-12-15 18:55:42 +0600

    [REFACTOR]: Rename AppColors to ColorUtils

--- lib/core/const/text_style.dart
+++ lib/core/const/text_style.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:scube/config/font_constant.dart';
-import 'app_colors.dart';
+import 'color_utils.dart';
 
 
 TextStyle customTextStyle(context,{Color? color,double? fontSize,fontWeight}){
   return  TextStyle(
       fontSize: fontSize ?? TextSize.font14(context),
       fontWeight: fontWeight ?? FontWeight.w500,
-      color: color ?? AppColors.baseColor
+      color: color ?? ColorUtils.baseColor
   );
 }
