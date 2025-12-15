fd4bc03e3499981c349e13f66c0a100e93248080
Tanjid1470 <tanjid@tutorsplan.com>
2025-12-15 23:51:11 +0600

    [REFACTOR]: Rename home view to scm view

--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
-import 'package:scube/feature/home/presentation/home_view.dart';
+import 'package:scube/feature/scm/presentation/scm_view.dart';
 import 'package:scube/feature/login/presentation/login_view.dart';
 import 'package:scube/main.dart';
 part 'app_routes.dart';
@@ -10,12 +10,12 @@ class AppPages{
 
   static String INITIAL = isInitScreen == 0 || isInitScreen == null
       ? RouteNames.loginView
-      : RouteNames.homeView;
+      : RouteNames.scmView;
 
   static final routes =[
     GetPage(
-      name:_Paths.homeView,
-      page: () => const HomeView(),
+      name:_Paths.scmView,
+      page: () => const SCMView(),
       transition: Transition.rightToLeftWithFade,
       curve: Curves.fastOutSlowIn,
     ),
--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -4,11 +4,11 @@ abstract class RouteNames {
   RouteNames._();
 
   static const loginView = _Paths.loginView;
-  static const homeView = _Paths.homeView;
+  static const scmView = _Paths.scmView;
 }
 
 abstract class _Paths {
   _Paths._();
   static const loginView = '/login_view';
-  static const homeView = '/home_view';
+  static const scmView = '/home_view';
 }
