62f71a40da2ada8ce39e5bb482d297e1da9a93b4
Tanjid1470 <tanjid@tutorsplan.com>
2025-12-15 18:54:59 +0600

    [REFACTOR]: Rename AppColors to ColorUtils and add new color constants

--- lib/core/const/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors{
-  AppColors._();
-  static Color baseColor = Color(0XFF0096FC);
-  static Color greyColor =  Colors.grey;
-  static Color black =  Colors.black;
-
-}
---
+++ lib/core/const/color_utils.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorUtils{
+  ColorUtils._();
+  static Color baseColor = Color(0XFF0096FC);
+  static Color greyColor =  Colors.grey;
+  static Color grey300 = Colors.grey.shade300;
+  static Color black =  Colors.black;
+
+  static Color inputBorderColor = Colors.grey.shade300;
+  static Color errorBorderColor = Colors.red;
+  static Color competeBorderColor = baseColor;
+
+}
