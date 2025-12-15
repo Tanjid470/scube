deb166bb6a7d6fc3c7a980786219f182a75650f6
Tanjid1470 <tanjid@tutorsplan.com>
2025-12-15 16:31:52 +0600

    [FEAT]: Add app routes and pages configuration

---
+++ lib/route/app_pages.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/animation.dart';
+part 'app_routes.dart';
+
+class AppPages{
+  AppPages._();
+  //
+  // static String INITIAL = isWeb == true
+  //     ? RouteNames.webApp
+  //     : isInitScreen == 0 || isInitScreen == null
+  //     ? RouteNames.onboardView
+  //     : RouteNames.bottomNavigationWidget;
+  //
+  // static final routes =[
+  //   GetPage(
+  //     name:_Paths.bottomNavigationWidget,
+  //     page: () => const BottomNavigationWidget(),
+  //     transition: Transition.leftToRight,
+  //     curve: Curves.fastOutSlowIn,
+  //   ),
+  // ];
+}
---
+++ lib/route/app_routes.dart
@@ -0,0 +1,12 @@
+part of 'app_pages.dart';
+
+abstract class RouteNames {
+  RouteNames._();
+
+  static const loginView = _Paths.loginView;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const loginView = '/login_view';
+}
