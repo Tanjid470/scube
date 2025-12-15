10e0f9ce79e959cfe4e5285baa5282818eceb103
Tanjid1470 <tanjid@tutorsplan.com>
2025-12-16 02:08:49 +0600

    [FEAT]: Make `ColoredCircle` widget responsive

--- lib/feature/scm_data_details/presentation/widget/multi_layer_circle.dart
+++ lib/feature/scm_data_details/presentation/widget/multi_layer_circle.dart
@@ -2,31 +2,36 @@ import 'package:flutter/material.dart';
 
 class ColoredCircle extends StatelessWidget {
   final Color color;
+  final double size; // total outer circle size
 
-  const ColoredCircle({super.key, required this.color});
+  const ColoredCircle({super.key, required this.color, this.size = 15});
 
   @override
   Widget build(BuildContext context) {
+    final outerSize = size;
+    final innerSize = size * 2 / 3; // inner circle = 2/3 of outer
+    final offset = (outerSize - innerSize) / 2;
+
     return Stack(
       children: [
         Container(
-          width: 15,
-          height: 15,
+          width: outerSize,
+          height: outerSize,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             color: Colors.white,
             border: Border.all(
               color: color,
-              width: 1,
+              width: outerSize * 0.07, // border proportional to size
             ),
           ),
         ),
         Positioned(
-          top: 2.5,
-          left: 2.5,
+          top: offset,
+          left: offset,
           child: Container(
-            width: 10,
-            height: 10,
+            width: innerSize,
+            height: innerSize,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               color: color,
